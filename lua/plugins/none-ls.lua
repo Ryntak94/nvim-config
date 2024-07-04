ed45045bdf7b11f650a3275b3f19549e78d49d50
Ryan Matthews <ryan.matthews.professional@gmail.com>
2024-07-04 14:37:00 -0500

    added more lsp's, added :noh keymap, fixed treesitter bug

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,21 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        require("none-ls.diagnostics.eslint"),
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				require("none-ls.diagnostics.eslint"),
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.code_actions.gomodifytags,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-    ensure_installed = {"lua", "javascript", "vimdoc"},
-    highlight = { enabled = true },
-    indent = { enabled = true }
-  })
-end
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"bash",
+				"diff",
+				"regex",
+				"lua",
+				"javascript",
+				"typescript",
+				"vimdoc",
+				"go",
+				"gosum",
+				"gomod",
+				"gowork",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
