00e7efa7488561f1aa98ea63315c208d2d1b15b6
Ryan Matthews <ryan.matthews.professional@gmail.com>
2024-07-04 14:35:13 -0500

    this makes webdevicons work. The color scheme overwrites them.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,7 +1,11 @@
 return {
-  "catppuccin/nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme catppuccin]])
-  end,
+	"catppuccin/nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme catppuccin]])
+		vim.api.nvim_set_hl()
+	end,
 }
