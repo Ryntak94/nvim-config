c667c320e2334e5318fe95e82d875f4b8bf0f00b
Ryan Matthews <ryan.matthews.professional@gmail.com>
2024-07-05 00:34:49 -0500

    remove comments

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -9,14 +9,12 @@ return {
 		vim.keymap.set("n", "gt", ":BufferNext<CR>")
 		vim.keymap.set("n", "gT", ":BufferPrevious<CR>")
 		vim.keymap.set("n", "gmt", ":BufferMoveNext<CR>")
+
 		vim.keymap.set("n", "gmT", ":BufferMovePrevious<CR>")
 		vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>")
 	end,
 	opts = {
-		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
 		animation = false,
-		-- insert_at_start = true,
-		-- …etc.
 	},
 	version = "^1.0.0", -- optional: only update when a new 1.x version is released
 }
